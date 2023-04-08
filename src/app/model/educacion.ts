47b69df8936ecbff224df8597b30b5c6d6cadc3c
Fernando Hasperue <fernando.hasperue@gmail.com>
2023-04-08 17:07:12 -0300

    Edicion componente Educacion

---
+++ src/app/model/educacion.ts
@@ -0,0 +1,11 @@
+export class Educacion {
+    id?: number;
+    nombreE: string;
+    descripcionE: string;
+
+    constructor(nombreE: string, descripcionE: string){
+        this.id = 2;
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+}
